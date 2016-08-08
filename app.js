ac3aa0266f021f61bda1eca0fd686a74195c0205
FantasyGao <1136519360@qq.com>
2016-08-07 23:41:20 -0700

    nice3

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var admin = require('./routes/admin');
 
 var settings = require('./settings');
